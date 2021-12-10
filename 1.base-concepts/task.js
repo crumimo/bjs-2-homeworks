e0a97d508524694e33873467b71d2154402776e0
Linda Blomniece <blomniecelinda@gmail.com>
2021-12-10 13:03:21 +0200

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,24 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  "use strict";
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d == 0) {
+    arr.push(-b / (2 * a));
+    console.log(arr);
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    console.log(arr);
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+
 
   return totalAmount;
 }
