99150e6a7085f18840fc691cba9a97753867269b
Linda Blomniece <blomniecelinda@gmail.com>
2021-12-19 13:34:49 +0200

    homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
@@ -9,7 +9,8 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  const arr2 = arr.filter(value => value > 0 && !(value % 3));
+  resultArr = arr2.map((value) => value * 10); 
 
   return resultArr; // array
 }
