a07c53afbd75b24490379cd8499bb07b459642c0
Linda Blomniece <blomniecelinda@gmail.com>
2021-12-24 15:17:22 +0200

    homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name,
+  this.gender = gender,
+  this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (addMark) {
+  if(this.marks === undefined){ 
+    this.marks = [addMark];
+  } else {
+    this.marks.push(addMark);
+  }
+}
+
+Student.prototype.addMarks = function (...addMarks) {
+  if(this.marks === undefined){ 
+    this.marks = [...addMarks];
+  } else {
+    this.marks.push(...addMarks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let value of this.marks) {
+    sum += value;
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
